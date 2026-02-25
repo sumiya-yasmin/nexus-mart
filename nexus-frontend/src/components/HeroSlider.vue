380c1859f38319e7422e9a364efd484c3923c105
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-25 20:10:12 +0600

    Add auto play functionalities for slider

--- nexus-frontend/src/components/HeroSlider.vue
+++ nexus-frontend/src/components/HeroSlider.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 import HeroCard from "./HeroCard.vue";
 import { heroSlides } from "@/data/heroSlides";
 const slides = ref(heroSlides);
@@ -11,6 +11,13 @@ const prevSlide = () => {
   currentIndex.value =
     currentIndex.value == 0 ? slides.value.length - 1 : currentIndex.value - 1;
 };
+let autoPlayInterval= null;
+onMounted(()=>{
+  autoPlayInterval=setInterval(nextSlide,3000);
+});
+onUnmounted(()=>{
+  clearInterval(autoPlayInterval);
+})
 </script>
 <template>
   <div class="relative w-full max-w-4xl mx-auto overflow-hidden group">
@@ -39,7 +46,7 @@ const prevSlide = () => {
     </button>
     <div class="absolute bottom-4 z-10 left-1/2 -translate-x-1/2 flex space-x-2">
        <div v-for="_slide,index in slides" :key="index"
-       class="w-2 h-2 rounded-full "
+       class="w-2.5 h-2.5 rounded-full cursor-pointer "
        :class="currentIndex==index?'bg-blue-400':'bg-white'"
        @click="currentIndex=index"
        >       
