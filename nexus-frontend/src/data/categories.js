6ca9eb36169c7dce76d611e42333cabe49247ceb
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-25 23:44:30 +0600

    Add categories mock data

---
+++ nexus-frontend/src/data/categories.js
@@ -0,0 +1,77 @@
+export const categoriesList = [
+  {
+    id: 1,
+    name: "Smartphones",
+    slug: "smartphones",
+    image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=500&auto=format&fit=crop",
+  },
+  {
+    id: 2,
+    name: "Laptops",
+    slug: "laptops",
+    image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=500&auto=format&fit=crop",
+  },
+  {
+    id: 3,
+    name: "Smartwatches",
+    slug: "smartwatches",
+    image: "https://images.unsplash.com/photo-1546868871-7041f2a55e12?q=80&w=500&auto=format&fit=crop",
+  },
+  {
+    id: 4,
+    name: "Audio",
+    slug: "audio",
+
+    image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=500&auto=format&fit=crop",
+  },
+  {
+    id: 5,
+    name: "Gaming",
+    slug: "gaming",
+    image: "https://images.unsplash.com/photo-1600080972464-8e5f35f63d08?q=80&w=500&auto=format&fit=crop",
+  },
+  {
+    id: 6,
+    name: "Accessories",
+    slug: "accessories",
+    image: "https://images.unsplash.com/photo-1528795259021-d8c86e14354c?q=80&w=500&auto=format&fit=crop",
+  },
+  
+  
+  {
+    id: 7, 
+    name: "Smartphones",
+    slug: "smartphones",
+    image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=500&auto=format&fit=crop",
+  },
+  {
+    id: 8,
+    name: "Laptops",
+    slug: "laptops",
+    image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=500&auto=format&fit=crop",
+  },
+  {
+    id: 9,
+    name: "Smartwatches",
+    slug: "smartwatches",
+    image: "https://images.unsplash.com/photo-1546868871-7041f2a55e12?q=80&w=500&auto=format&fit=crop",
+  },
+  {
+    id: 10,
+    name: "Audio",
+    slug: "audio",
+    image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=500&auto=format&fit=crop",
+  },
+  {
+    id: 11,
+    name: "Gaming",
+    slug: "gaming",
+    image: "https://images.unsplash.com/photo-1600080972464-8e5f35f63d08?q=80&w=500&auto=format&fit=crop",
+  },
+  {
+    id: 12,
+    name: "Accessories",
+    slug: "accessories",
+    image: "https://images.unsplash.com/photo-1528795259021-d8c86e14354c?q=80&w=500&auto=format&fit=crop",
+  }
+];
