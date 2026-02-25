da01e7e307ff0b71392409735cbe6232c76a7ad8
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-25 14:44:40 +0600

    Add Hero slider component

---
+++ nexus-frontend/src/components/HeroSlider.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HeroCard from './HeroCard.vue';
+import { heroSlides } from '@/data/heroSlides';
+const slides = heroSlides;
+</script>
+<template>
+    <div class="px-12 flex items-center justify-center flex-col gap-4">
+        <HeroCard v-for="slide in slides" :key="slide.id" :slide="slide"/>
+    </div>
+</template>
