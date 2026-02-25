f371f54d11c74fdb9ee46754db2795520b7e6537
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-25 14:45:01 +0600

    Add Hero slider in App.vue

--- nexus-frontend/src/App.vue
+++ nexus-frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import HeroSlider from "./components/HeroSlider.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <Navbar />
+  <HeroSlider />
 </template>
 
 <style scoped></style>
