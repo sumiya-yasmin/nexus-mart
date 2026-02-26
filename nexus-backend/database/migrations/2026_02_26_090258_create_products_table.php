0ad3ad280449654aca20c33b6cf234fbb39accf5
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-26 15:59:07 +0600

    Backend: Add product migration table

---
+++ nexus-backend/database/migrations/2026_02_26_090258_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->decimal('price',12,2);
+            $table->decimal('old_price',12,2)->nullable();
+            $table->boolean('is_flash_sale')->default(false);
+            $table->string('image')->nullable();
+            $table->float('rating',2,1)->default(0.0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
