02863555f18a72993304bc578064092f5d1772a1
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-26 13:11:43 +0600

    Add Main Layout component

---
+++ nexus-frontend/src/Layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Footer from "@/components/Footer.vue";
+import Navbar from "../components/Navbar.vue";
+</script>
+
+<template>
+  <div class="min-h-screen bg-slate-50 flex flex-col">
+    <Navbar />
+
+    <main class="flex-grow">
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
