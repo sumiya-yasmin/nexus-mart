87223c00c8e5771c7df1280070ed149c38cd1247
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-26 12:27:29 +0600

    Add featured product section and render products using product card

---
+++ nexus-frontend/src/components/FeaturedProducts.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+import ProductCard from './ProductCard.vue';
+import { productList } from '@/data/product';
+const products = ref(productList);
+</script>
+<template>
+    <div class="max-w-7xl mx-auto px-4 pb-12 mt-12">
+<h2 class="text-2xl font-bold mb-10 text-center">Featured Products</h2>    
+<div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <ProductCard 
+            v-for="product in products" 
+            :key="product.id" 
+            :product="product"
+        />
+    </div>
+    </div>
+</template>
