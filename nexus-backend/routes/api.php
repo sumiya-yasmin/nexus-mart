f89b7a0533d7429783544a5b61b83bd6388577bc
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-26 22:10:51 +0600

    install laravel sanctum and publish api routes

---
+++ nexus-backend/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);
