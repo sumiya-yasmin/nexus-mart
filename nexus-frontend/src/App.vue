1a633b567325596c88b93fc3cc4b722824822714
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-26 12:27:58 +0600

    Add featured product section to app

--- nexus-frontend/src/App.vue
+++ nexus-frontend/src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
 import CategoryRow from "./components/CategoryRow.vue";
+import FeaturedProducts from "./components/FeaturedProducts.vue";
 import HeroSlider from "./components/HeroSlider.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
+  <div class="min-h-screen bg-slate-50">
   <Navbar />
   <HeroSlider />
   <CategoryRow />
+  <FeaturedProducts />
+  </div>
 </template>
 
 <style scoped></style>
