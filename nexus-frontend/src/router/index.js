b2a2df56da65b6c375c8fdf7ffcf48fcb1dbcc64
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-27 09:57:49 +0600

    Confgure vue-router

---
+++ nexus-frontend/src/router/index.js
@@ -0,0 +1,11 @@
+import Home from "@/pages/Home.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+    {path: '/', component: Home},
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+export default router;
