ec3e99f8aabb494b80706846f3540b6448864f36
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-27 10:00:14 +0600

    Migrate home contentfrom app to deticated home page

---
+++ nexus-frontend/src/pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import CategoryRow from '@/components/CategoryRow.vue';
+import FeaturedProducts from '@/components/FeaturedProducts.vue';
+import HeroSlider from '@/components/HeroSlider.vue';
+
+</script>
+<template>
+    <HeroSlider />
+    <CategoryRow />
+    <FeaturedProducts />
+</template>
