cdeb61970e377aba4ded6bcf64a0f9a25806fab3
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-27 16:03:59 +0600

    Add route for product detail page

--- nexus-frontend/src/router/index.js
+++ nexus-frontend/src/router/index.js
@@ -1,8 +1,10 @@
 import Home from "@/pages/Home.vue";
+import ProductDetail from "@/pages/ProductDetail.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-    {path: '/', component: Home},
+    { path: '/', component: Home },
+    { path: '/product/:slug', component: ProductDetail, props: true  },
 ]
 const router = createRouter({
     history: createWebHistory(),
