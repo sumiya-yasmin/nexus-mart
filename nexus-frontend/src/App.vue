1f3b11e212d97aafd530f45708bd6b47a4038d17
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-27 10:03:30 +0600

    update App.vue to use router-view within MainLayout

--- nexus-frontend/src/App.vue
+++ nexus-frontend/src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
-import CategoryRow from "./components/CategoryRow.vue";
-import FeaturedProducts from "./components/FeaturedProducts.vue";
-import HeroSlider from "./components/HeroSlider.vue";
-import Navbar from "./components/Navbar.vue";
 import MainLayout from "./Layouts/MainLayout.vue";
 </script>
 
 <template>
   <MainLayout> 
-
-  <HeroSlider />
-  <CategoryRow />
-  <FeaturedProducts />
+<router-view />
   </MainLayout>
 </template>
 
