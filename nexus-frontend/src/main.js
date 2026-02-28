c7042cdc6e88e9fb8a4f9d1e2f1f9528d378e467
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-28 18:24:50 +0600

    Configure toast

--- nexus-frontend/src/main.js
+++ nexus-frontend/src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import './assets/main.css'
 import router from './router';
 import { createPinia } from 'pinia';
+import Toast from 'vue-toastification';
+import "vue-toastification/dist/index.css";
 
 const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia);
 app.use(router);
+app.use(Toast);
 app.mount('#app');
