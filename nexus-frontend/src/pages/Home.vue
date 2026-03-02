7098f4b33529b5494dd30e8e3fa58b7cda4af814
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-02 15:56:51 +0600

    add master reset logic to clear both categories and search

--- nexus-frontend/src/pages/Home.vue
+++ nexus-frontend/src/pages/Home.vue
@@ -2,10 +2,15 @@
 import CategoryRow from '@/components/CategoryRow.vue';
 import FeaturedProducts from '@/components/FeaturedProducts.vue';
 import HeroSlider from '@/components/HeroSlider.vue';
+import { useProductStore } from '@/store/product';
 import { ref } from 'vue';
 
 const currentFilter = ref('');
-
+const productStore = useProductStore();
+const masterReset = () => {
+    currentFilter.value = '';
+    productStore.searchQuery = '';
+}
 </script>
 <template>
     <HeroSlider />
@@ -13,6 +18,6 @@ const currentFilter = ref('');
     :activeFilter="currentFilter"
     @category-filter="(slug)=> currentFilter=slug"/>
     <FeaturedProducts :activeFilter="currentFilter"
-    @clear-filter="currentFilter=''"
+    @clear-filter="masterReset"
     />
 </template>
