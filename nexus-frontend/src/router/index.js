443f9aa30f1278e151a613c50f271757d3cfa7aa
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-03 16:03:55 +0600

    Add register page, login page routes

--- nexus-frontend/src/router/index.js
+++ nexus-frontend/src/router/index.js
@@ -1,12 +1,17 @@
 import CartView from "@/pages/CartView.vue";
 import Home from "@/pages/Home.vue";
+import Login from "@/pages/Login.vue";
 import ProductDetail from "@/pages/ProductDetail.vue";
+import Register from "@/pages/Register.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/product/:slug", component: ProductDetail, props: true },
   { path: "/cart", component: CartView },
+  { path: "/register", component: Register },
+    {path: "/login", component: Login},
+
 ];
 const router = createRouter({
   history: createWebHistory(),
