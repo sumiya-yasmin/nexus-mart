e0c21ee7b42c7efedc48ab0d090c810f21e1faa7
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-03 16:03:36 +0600

    Add register page

---
+++ nexus-frontend/src/pages/Register.vue
@@ -0,0 +1,24 @@
+<script setup>
+import AuthForm from '@/components/AuthForm.vue';
+import { useAuthStore } from '@/store/auth';
+import { useRouter } from 'vue-router';
+const authStore = useAuthStore();
+const router = useRouter();
+const handleRegister = async(data) => {
+    try {
+        await authStore.register(data);
+        router.push('/');
+    } catch (error) {
+        alert('Registration failed', error);
+    }
+}
+</script>
+
+<template>
+    <div class="mt-8">
+        <AuthForm 
+         mode="register"
+         @submit="handleRegister"
+        />
+    </div>
+</template>
