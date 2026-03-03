2396680341b713b35cf618affcf3bcdb1fe46826
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-03 16:02:05 +0600

    Add auth store

---
+++ nexus-frontend/src/store/auth.js
@@ -0,0 +1,40 @@
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+import * as authApi from "@/api/authService";
+export const useAuthStore = defineStore("auth", () => {
+  const user = ref(JSON.parse(localStorage.getItem("user")) || null);
+  const token = ref(localStorage.getItem("token") || null);
+
+  const isLoggedIn = computed(() => !!token.value);
+  const isAdmin = computed(() => {
+    return user.value?.role === "admin";
+  });
+
+  const login = async (credentials) => {
+    try {
+        const response = await authApi.login(credentials);
+      user.value = response.data.user;
+      token.value = response.data.token;
+      localStorage.setItem("user", JSON.stringify(user.value));
+      localStorage.setItem("token", token.value);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const register = async (userData) => {
+    try {
+        const response = await authApi.register(userData);
+      user.value = response.data.user;
+      token.value = response.data.token;
+      localStorage.setItem("user", JSON.stringify(user.value));
+      localStorage.setItem("token", token.value);
+    } catch (error) {
+      throw error;
+    }
+  };
+    return {
+        user, token, isLoggedIn, isAdmin, login,register
+    }
+});
