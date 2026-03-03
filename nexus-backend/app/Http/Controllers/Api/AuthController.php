c010f0944a1162b871933908ff965f0b9fba8cec
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-03 16:00:42 +0600

    Add login, register controller

---
+++ nexus-backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name'=> 'required|string',
+            'email'=> 'required|unique:users,email',
+            'password'=> 'required|string|confirmed',
+        ]);
+
+        $user = User::create([
+            'name'=> $fields['name'],
+            'email'=> $fields['email'],
+            'password'=> bcrypt($fields['password']),
+            'role'=> 'customer',
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+        return response([
+            'user'=> $user,
+            'Token'=> $token,
+        ],201);
+
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email'=> 'required',
+            'password'=> 'required',
+        ]);
+
+        $user = User::where([
+            'email'=> $fields['email'],        
+        ])->first();
+
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+                return response([
+                    'message'=> 'Invalid credentials',
+                ],401);
+        }
+        $token = $user->createToken('myapptoken')->plainTextToken;
+        return response([
+            'user'=> $user,
+            'Token'=> $token,
+        ],201);
+    }
+}
