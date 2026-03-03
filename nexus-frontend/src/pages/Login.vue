b93adf51e82a40993894b547f33802872d7c13f1
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-03 16:03:21 +0600

    Add Login page

---
+++ nexus-frontend/src/pages/Login.vue
@@ -0,0 +1,22 @@
+<script setup>
+import AuthForm from "@/components/AuthForm.vue";
+import { useAuthStore } from "@/store/auth";
+import { useRouter } from "vue-router";
+
+const authStore = useAuthStore();
+const router = useRouter();
+const handleLogin = async(data) => {
+    try {
+        await authStore.login(data);
+        router.push('/');
+    } catch (error) {
+        alert('Login failed', error);
+
+    }
+}
+</script>
+<template>
+  <div class="mt-8">
+    <AuthForm mode="login" @submit="handleLogin" />
+  </div>
+</template>
