f0e05d828f01f705631c6f71ed0447a8f480c5ae
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-04 15:59:27 +0600

    Add order controller

---
+++ nexus-backend/app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\OrderItem;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class OrderController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'items' => 'required|array',
+            'total_price' => 'required|numeric',
+            'address' => 'required|string'
+        ]);
+
+        return DB::transaction(function () use ($request) {
+            $order = Order::create([
+                'user_id' => $request->user()->id(),
+                'total_price' => $request->total_price,
+                'status' => 'pending',
+                'address' => $request->address,
+            ]);
+
+            foreach ($request->items as $item) {
+                OrderItem::create([
+                    'order_id' => $order->id,
+                    'product_id' => $item['id'],
+                    'quantity' => $item['quantity'],
+                    'price' => $item['price'],
+                ]);
+            };
+            return response()->json([
+                'message' => 'Order created successfully!',
+                'order_id' => $order->id
+            ], 201);
+        });
+    }
+}
