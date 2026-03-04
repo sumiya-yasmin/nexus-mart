6ffdf08e67b737d45f1bc1582c51365173e43c1f
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-04 10:28:04 +0600

    Add logout routes using protected route grouping

--- nexus-backend/routes/api.php
+++ nexus-backend/routes/api.php
@@ -17,3 +17,6 @@ Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
