5c64e2f04802fd393771ac51c6788c2a665c3b59
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-04 11:15:19 +0600

    Add authguard to add protected routes

--- nexus-frontend/src/pages/Login.vue
+++ nexus-frontend/src/pages/Login.vue
@@ -1,25 +1,26 @@
 <script setup>
 import AuthForm from "@/components/AuthForm.vue";
 import { useAuthStore } from "@/store/auth";
-import { useRouter } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { useToast } from "vue-toastification";
 
 const authStore = useAuthStore();
 const router = useRouter();
+const route = useRoute();
 const toast = useToast();
-const handleLogin = async(data) => {
-    try {
-      await authStore.login(data);
-      toast.success(`Login successful`, {
+const handleLogin = async (data) => {
+  try {
+    await authStore.login(data);
+    toast.success(`Login successful`, {
       timeout: 2000,
       position: "top-right",
     });
-        router.push('/');
-    } catch (error) {
-        alert('Login failed', error);
-
-    }
-}
+    const redirectPath = route.query.redirect || "/";
+    router.push(redirectPath);
+  } catch (error) {
+    alert("Login failed", error);
+  }
+};
 </script>
 <template>
   <div class="mt-8">
--- nexus-frontend/src/router/index.js
+++ nexus-frontend/src/router/index.js
@@ -3,18 +3,31 @@ import Home from "@/pages/Home.vue";
 import Login from "@/pages/Login.vue";
 import ProductDetail from "@/pages/ProductDetail.vue";
 import Register from "@/pages/Register.vue";
+import { useAuthStore } from "@/store/auth";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/product/:slug", component: ProductDetail, props: true },
-  { path: "/cart", component: CartView },
+  { path: "/cart", component: CartView, meta: { requiresAuth: true } },
   { path: "/register", component: Register },
-    {path: "/login", component: Login},
+  {path: "/login", component: Login, meta: { guestOnly: true }},
 
 ];
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  if (to.meta.requiresAuth && !authStore.isLoggedIn) {
+    next({ path: '/login', query: { redirect: to.fullPath } });
+  } else if (to.meta.guestOnly && authStore.isLoggedIn) {
+    next('/');
+  } else {
+    next();
+  }
+
+
+})
 export default router;
