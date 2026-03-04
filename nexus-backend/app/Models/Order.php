21caca3b68f7adca9af7c3aca36932a68d0ae14b
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-04 15:58:11 +0600

    Add Order, OrderItem model

---
+++ nexus-backend/app/Models/Order.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['user_id', 'total_price', 'status', 'address'];
+    public function items (){
+        return $this->hasMany(OrderItem::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+}
