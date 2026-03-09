2d85919251524a5ec1285a61ef0c55064ee32485
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-09 13:11:11 +0600

    Implement advanced price-range based product filtering in ProductController,add pagination and price range metadata to product index

--- nexus-backend/app/Http/Controllers/Api/ProductController.php
+++ nexus-backend/app/Http/Controllers/Api/ProductController.php
@@ -4,23 +4,35 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products =  Product::query()->with('category')->get();
+        $query = Product::query()->with('category');
+        $query->when($request->category_id, fn($q) => $q->where('category_id', $request->category_id) );
+        $query->when($request->min_price, fn($q)=>$q->where('price', '>=', $request->min_price));
+        $query->when($request->max_price, fn($q) => $q->where('price', '<=', $request->max_price));
+        $products =  $query->latest()->paginate(12);
+
+        $meta = [
+            'min_db_price' => (float) Product::min('price') ?? 0,
+            'max_db_price' => (float) Product::max('price') ?? 0,
+        ];
+
         return response()->json([
             'success' => true,
-            'data' => $products
+            'data' => $products,
+            'meta' => $meta,
         ], 200);
     }
 
-    public function show(Product $product)
+    public function show(Product $product) 
     {
         return response()->json([
             'success' => true,
-            'data' => $product
+            'data' => $product->load('category')
         ], 200);
     }
 }
