0b7b68829f2d1ae126e1c6aa548f0d8b380b84a4
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-11 11:52:54 +0600

    Add admin user api services in frontend

---
+++ nexus-frontend/src/api/admin/adminUserService.js
@@ -0,0 +1,14 @@
+export const getAllUsers = async () => {
+    const response = await client.get('/admin/users');
+    return response.data;
+};
+
+export const updateUser = async (userId, userData) => {
+    const response = await client.patch(`/admin/users/${userId}`, userData);
+    return response.data;
+};
+
+export const deleteUser = async (userId) => {
+    const response = await client.delete(`/admin/users/${userId}`);
+    return response.data;
+};
