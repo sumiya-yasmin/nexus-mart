0da3ecb085af8b3dfe78a1528c498133a8bd27a1
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-11 14:40:08 +0600

    Add Order Table component in Order Page

---
+++ nexus-frontend/src/components/admin/OrderTable.vue
@@ -0,0 +1,95 @@
+<script setup>
+import { useAdminOrders } from "@/composable/useAdminOrders";
+import { onMounted } from "vue";
+const {
+  loading,
+  error,
+  orders,
+  fetchOrders,
+  updateStatus,
+  updatePaymentStatus,
+} = useAdminOrders();
+onMounted(() => {
+  fetchOrders();
+});
+</script>
+<template>
+  <div class="p-6 bg-white rounded-lg shadow-sm">
+    <h2 class="text-2xl font-bold mb-4">NexusMart Orders</h2>
+
+    <div v-if="loading" class="text-rose-500">Updating inventory...</div>
+    <table class="w-full text-left border-collapse">
+      <thead>
+        <tr class="border-b bg-gray-50">
+          <th class="p-3">Order Id</th>
+          <th class="p-3">Customer</th>
+          <th class="p-3">Total</th>
+          <th class="p-3">Payment Status</th>
+          <th class="p-3">Status</th>
+          <th class="p-3 text-center">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="order in orders"
+          :key="order.id"
+          class="border-b hover:bg-gray-50"
+        >
+          <td class="p-3 font-mono">#NEXUS-{{ order.id }}</td>
+          <td class="p-3">{{ order.user?.name }}</td>
+          <td class="p-3">{{ order.total_price }}</td>
+          <td class="p-3">
+            <span
+              :class="{
+                'px-2 py-1 rounded text-xs font-bold uppercase': true,
+                'bg-yellow-100 text-yellow-700': order.payment_status === 'unpaid',
+                'bg-green-100 text-green-700': order.payment_status === 'paid',
+              }"
+            >
+              {{ order.payment_status }}
+            </span>
+          </td>
+          <td class="p-3">
+            <span
+              :class="{
+                'px-2 py-1 rounded text-xs font-bold uppercase': true,
+                'bg-yellow-100 text-yellow-700': order.status === 'pending',
+                'bg-orange-100 text-orange-700': order.status === 'processing',
+                'bg-blue-100 text-blue-700': order.status === 'shipped',
+                'bg-green-100 text-green-700': order.status === 'delivered',
+                'bg-red-100 text-red-700': order.status === 'cancelled',
+              }"
+            >
+              {{ order.status }}
+            </span>
+          </td>
+          <td class="p-3">
+            <select
+              class="border rounded p-1 text-sm bg-white"
+              @change="updatePaymentStatus(order.id, $event.target.value)"
+              :value="order.payment_status"
+            >
+              <option>Change Payment Status</option>
+              <option value="unpaid">Unpaid</option>
+              <option value="paid">Paid</option>
+            </select>
+          </td>
+          <td class="p-3">
+            <select
+              class="border rounded p-1 text-sm bg-white"
+              @change="updateStatus(order.id, $event.target.value)"
+              :value="order.status"
+            >
+              <option>Change Status</option>
+              <option value="pending">Pending</option>
+              <option value="processing">Processing</option>
+              <option value="shipped">Shipped</option>
+              <option value="delivered">Delivered</option>
+              <option value="cancelled">Cancelled</option>
+            </select>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
---
+++ nexus-frontend/src/pages/admin/Order.vue
@@ -0,0 +1,7 @@
+<script setup>
+import OrderTable from '@/components/admin/OrderTable.vue';
+
+</script>
+<template>
+    <OrderTable />
+</template>
