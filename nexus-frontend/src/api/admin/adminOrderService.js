ce6b0189a116fa382f35fc584577689a98ab3650
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-11 14:39:16 +0600

    Fix admin order service bug

--- nexus-frontend/src/api/admin/adminOrderService.js
+++ nexus-frontend/src/api/admin/adminOrderService.js
@@ -1,13 +1,17 @@
 import client from "../client";
 
-export const getAllOrders = async () =>
-{
-    return await client.get('/admin/orders');
-}
-export const updateOrderStatus =async(orderId, status)=>{
-    return client.patch(`/admin/orders/${orderId}/status`, {status});
-}
+export const getAllOrders = async () => {
+  const response = await client.get("/admin/orders");
+  return response.data;
+};
+export const updateOrderStatus = async (orderId, status) => {
+  const response = client.patch(`/admin/orders/${orderId}/status`, { status });
+  return response.data;
+};
 
-export const updateOrderPaymentStatus =  async(orderId, paymentStatus) =>{
-    return client.patch(`/admin/orders/${orderId}/payment_status`, {payment_status: paymentStatus});
-}
+export const updateOrderPaymentStatus = async (orderId, paymentStatus) => {
+  const response = client.patch(`/admin/orders/${orderId}/payment_status`, {
+    payment_status: paymentStatus,
+  });
+  return response.data;
+};
