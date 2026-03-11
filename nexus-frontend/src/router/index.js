89e1cade9b2d7e8776639198aec988088e25871d
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-11 14:37:08 +0600

    Fix routes bug

--- nexus-frontend/src/router/index.js
+++ nexus-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import AdminLayout from "@/Layouts/AdminLayout.vue";
 import MainLayout from "@/Layouts/MainLayout.vue";
 import Dashboard from "@/pages/admin/Dashboard.vue";
+import Order from "@/pages/admin/Order.vue";
 import CartView from "@/pages/CartView.vue";
 import Home from "@/pages/Home.vue";
 import Login from "@/pages/Login.vue";
@@ -35,10 +36,12 @@ const routes = [
   },
   {
     path: "/admin",
-    component: () => AdminLayout,
+    component: AdminLayout,
     meta: { requiresAuth: true, requiresAdmin: true },
     children: [
-      { path: "dashboard", name:"admin-dashboard", component: Dashboard },
+      { path: "dashboard", name: "admin-dashboard", component: Dashboard },
+      { path: "orders", name: "admin-orders", component: Order },
+      
     ]
   }
 ];
