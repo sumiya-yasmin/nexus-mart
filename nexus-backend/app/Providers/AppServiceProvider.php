fca040d7f6f88407d00b7e7c4dfc01f48c1f218b
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-03-27 15:56:32 +0600

    Add event in AppService.php

--- nexus-backend/app/Providers/AppServiceProvider.php
+++ nexus-backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Events\OrderPlaced;
+use App\Listeners\sendOrderNotification;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::listen(
+            OrderPlaced::class,
+            sendOrderNotification::class,
+        );
     }
 }
